e3854d4ded4bdec2d44e728bfc0e529aa68917ab
Usama Fazal <Usama Fazal@WIN-VOL3D49H6UK>
2022-06-29 00:43:23 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//using System.Linq for utilising functions of Max, Min, or Sum
+using System.Linq;
+using System;
+
+namespace QuestionNo5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 4, 5, 99, 33, 11, 34 };
+            // because of System.Linq we can directly use array.Max() to find the max value
+            Console.WriteLine("The maximum value in the array is " + numbers.Max());
+
+        }
+    }
+}
